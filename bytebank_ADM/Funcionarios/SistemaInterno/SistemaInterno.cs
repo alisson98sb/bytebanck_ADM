efffb276dd64b39eafdde951dcc76a250486345e
alissonpremium <alisson@premiumads.com.br>
2022-08-04 06:39:22 -0300

    Aula-05 Herança multipla

--- bytebank_ADM/Funcionarios/SistemaInterno/SistemaInterno.cs
+++ bytebank_ADM/Funcionarios/SistemaInterno/SistemaInterno.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using bytebank_ADM.ParceriaComercial;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,22 @@ namespace bytebank_ADM.Funcionarios.SistemaInterno
     public class SistemaInterno
     {
         public bool Logar(Autenticavel funcionario, string senha)
+        {
+            bool usuarioAutenticado = funcionario.Autenticar(senha);
+
+            if (usuarioAutenticado == true)
+            {
+                Console.WriteLine("Bem-vindo ao sistema");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Senha incorreta");
+                return false;
+            }
+        }
+
+        public bool Logar(ParceiroComercial funcionario, string senha)
         {
             bool usuarioAutenticado = funcionario.Autenticar(senha);
 
